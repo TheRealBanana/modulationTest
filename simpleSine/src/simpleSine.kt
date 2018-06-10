3d6c00b5b70c1f5477bebebf90fa1b547b0dadd5
Kyle <TheRealBanana@users.noreply.github.com>
2018-06-10 16:14:11 -0700

    I suck at github
    That workspace.xml file can go straight to hell. I didnt even touch the other projects code but apparently messing with the view inside IntelliJ changes those files. Prollly shouldnt have commited those.

--- simpleSine/src/simpleSine.kt
+++ simpleSine/src/simpleSine.kt
@@ -4,12 +4,21 @@ import org.lwjgl.opengl.GL
 import org.lwjgl.opengl.GL11
 import org.lwjgl.opengl.GL11.*
 import org.lwjgl.system.MemoryUtil
+import kotlin.math.PI
 import kotlin.math.sin
 
 //GL STUFFS
 const val WINDOW_SIZE_HEIGHT = 500
 const val WINDOW_SIZE_WIDTH = 500
+const val TIME_STEP = 0.001 //how much do we increment our x value each tick. Controls the integrity/continuity of the generated waveform
 var window: Long = NULL
+
+//WAVE STUFFS
+//Instead of using a frequency in hz, we are counting the number of full cycles visible on the screen
+const val AMPLITUDE = 2
+const val CYCLES: Double = 10.0
+const val PHASE_SHIFT_DEGREES: Double = 0.0 //Amount to shift the wave by in degrees (0-360). Negative number shift left, positive to the right.
+
 
 fun init(windowSizeW: Int = WINDOW_SIZE_WIDTH, windowSizeH: Int = WINDOW_SIZE_HEIGHT) {
     if ( !glfwInit()) {
@@ -27,7 +36,7 @@ fun init(windowSizeW: Int = WINDOW_SIZE_WIDTH, windowSizeH: Int = WINDOW_SIZE_HE
     // GL configuration comes AFTER we make the window our current context, otherwise errors
     GL.createCapabilities()
     GL11.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
-    GL11.glOrtho(-1.0, 1.0, -3.0, 3.0, -1.0, 1.0)
+    GL11.glOrtho(0.0, WINDOW_SIZE_WIDTH.toDouble(), -3.0, 3.0, -1.0, 1.0)
     GL11.glViewport(0, 0, WINDOW_SIZE_WIDTH, WINDOW_SIZE_HEIGHT)
     glfwShowWindow(window)
 }
@@ -39,14 +48,15 @@ private fun drawSine() {
         glfwPollEvents()
         glBegin(GL_POINTS)
         glClear(GL_COLOR_BUFFER_BIT)
-        val amplitude = 2.0
-        val cycles = 2.0 //not a 1:1 relationship between this val and the actual number of peaks/troughs on screen
 
-        var x: Double = -1.0
-        while (x < 1.0) {
-            val y: Double = amplitude * sin(cycles * x)
+        val timeperiod: Double = WINDOW_SIZE_WIDTH/CYCLES
+        val carrierfreq: Double = 1.0/timeperiod
+        val phaseshift: Double = PHASE_SHIFT_DEGREES * PI/180.0
+        var x: Double = 0.0
+        while (x < WINDOW_SIZE_WIDTH) {
+            val y: Double = AMPLITUDE * sin(2*PI * carrierfreq * x + phaseshift)
             glVertex2d(x, y)
-            x += 0.0001
+            x += TIME_STEP
         }
         glEnd()
         glfwSwapBuffers(window)
