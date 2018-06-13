f1592995836f10e3ac649e32fa5c96384c8b62a1
Kyle <TheRealBanana@users.noreply.github.com>
2018-06-12 19:42:11 -0700

    tiangles...
    Was going to do a semicircle too but the math for the first kind of bessier function is so far over my head its hilarious. I found a formula for determining the output of a specific order bessel function but Im not sure if its exactly what I need.  Will have to look into gamma functions as well. :(

--- fourierSeriesTriangleWave/src/fourierSeriesTriangleWave.kt
+++ fourierSeriesTriangleWave/src/fourierSeriesTriangleWave.kt
@@ -4,6 +4,7 @@ import org.lwjgl.opengl.GL
 import org.lwjgl.opengl.GL11
 import org.lwjgl.opengl.GL11.*
 import org.lwjgl.system.MemoryUtil
+import java.lang.Math.pow
 import kotlin.math.PI
 import kotlin.math.sin
 
@@ -15,7 +16,7 @@ var window: Long = NULL
 
 //WAVE STUFFS
 const val TIME_STEP = 0.001 //how much do we increment our x value each tick. Controls the integrity/continuity of the generated waveform
-const val CYCLES: Double = 4.0
+const val CYCLES: Double = 1.0
 const val TERM_STEP: Int = 1
 
 fun init(windowSizeW: Int = WINDOW_SIZE_WIDTH, windowSizeH: Int = WINDOW_SIZE_HEIGHT) {
@@ -25,7 +26,7 @@ fun init(windowSizeW: Int = WINDOW_SIZE_WIDTH, windowSizeH: Int = WINDOW_SIZE_HE
     glfwDefaultWindowHints()
     //Do not allow resize
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE)
-    window = glfwCreateWindow(windowSizeW, windowSizeH, "FourierSawtoothWave", 0, 0)
+    window = glfwCreateWindow(windowSizeW, windowSizeH, "FourierTriangleWave", 0, 0)
     if (window == MemoryUtil.NULL) {
         throw Exception("Failed to initialize window.")
     }
@@ -50,28 +51,26 @@ private fun drawSine() {
 
         var x = 0.0
         while (x < WINDOW_SIZE_WIDTH) {
-            if (x == 0.0) println("Creating sine waves using the fist $curstep terms of the fourier series...")
+            if (x == 0.0) println("Creating sine waves using the fist ${(curstep/2)+1} terms of the fourier series...")
             var finaly = 0.0
-            for (n in 1..curstep) {
+            for (n in 1..curstep step 2) {
                 val rightterm: Double = (n.toDouble() * PI * x)/(WINDOW_SIZE_WIDTH.toDouble()/(CYCLES*2))
-                val finalval: Double = (1.0/n.toDouble())*sin(rightterm)
+                val finalval: Double = (pow(-1.0, (n-1.0)/2.0)/pow(n.toDouble(), 2.0))*sin(rightterm)
                 finaly += finalval
             }
-            //sawtooth magics
-            finaly = 0.5-(1/PI) * finaly
-            glVertex2d(x, 4/PI*finaly)
+            glVertex2d(x, (8/(PI*PI))*finaly)
             x += TIME_STEP
         }
         glEnd()
         glfwSwapBuffers(window)
-        curstep += TERM_STEP
-        Thread.sleep(500)
+        curstep += TERM_STEP*2
+        Thread.sleep(2000)
     }
 }
 
 fun main(args: Array<String>) {
     init()
-    println("Generating a sawtooth wave using the Fourier series describing a sawtooth wave.")
+    println("Generating a triangle wave using the Fourier series describing a triangle wave.")
     drawSine()
 
 }
